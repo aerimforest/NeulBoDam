a9deb575e6820f25076c7b6bf59104f3bd4dd8c2
aerim <aerimforest98@gmail.com>
2021-07-03 02:11:42 +0900

    스플래시 액티비티 레이아웃 구성

---
+++ app/src/main/java/com/example/neulbodam/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.neulbodam
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+
+class SplashActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // status bar 색상 변경
+        val window = this.window
+        window.statusBarColor = ContextCompat.getColor(this, R.color.main_color)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
